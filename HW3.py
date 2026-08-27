6e6d500865d5dcab95a29083d4c335b05e25437a
Oliver Waldrop <ow130315@student.wpsok.org>
2026-08-27 12:50:54 -0500

    Submitted HW2

---
+++ HW3.py
@@ -0,0 +1,29 @@
+#Name: Oliver Waldrop
+#Class: 5th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+print("Hello World!")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+intVar1=7
+stringVar2="Oliver"
+booleanVar3=True
+#3. Print all three variables on the same print function (at the same time).
+print(intVar1, stringVar2, booleanVar3)
+#4. Create a variable that asks the user to input an integer.
+Favorite_number = int(input("What is your Favorite Number?"))
+print(Favorite_number)
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+print(intVar1 + Favorite_number)
+#6. Take the result from #5 and divide it by 2. Print the result.
+Wyatt=intVar1+Favorite_number
+print(Wyatt / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+booleanVar3 = False
+#8. Print the value of the boolean variable.
+print(booleanVar3)
+#9. Create a variable with a number that contains decimals.
+intVar4=2.12342345343435
+#10. Round the number from #9 up or down using the round function.
+print(round(2.12342345343435, 3))
